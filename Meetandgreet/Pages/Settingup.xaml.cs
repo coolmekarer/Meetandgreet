80cd9fb002078e10d800a8eaa454362198184abd
mekarer <mekarer30@gmail.com>
2026-04-16 10:30:41 +0300

    i made a change

--- Meetandgreet/Pages/Settingup.xaml.cs
+++ Meetandgreet/Pages/Settingup.xaml.cs
@@ -12,17 +12,33 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using interfaceapi;
+using ModelDates;
+using ViewModel;
+
 
 namespace Meetandgreet.Pages
 {
     /// <summary>
     /// Interaction logic for Settingup.xaml
     /// </summary>
-    public partial class Settingup : Page
+    public partial class SetupPage1 : Page
     {
-        public Settingup()
+        // Replace 'YourExistingViewModel' with the actual name of your class
+        private ViewModel viewModel;
+
+        public SetupPage1(ViewModel vm)
         {
             InitializeComponent();
+            _viewModel = vm;
+            // This is the magic line that connects the XAML to your code
+            this.DataContext = _viewModel;
+        }
+
+        private void NextStep_Click(object sender, RoutedEventArgs e)
+        {
+            // When moving to Step 2, pass the same VM along
+            NavigationService.Navigate(new Settingup2(_viewModel));
         }
     }
 }
